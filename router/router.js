2d7264f362062f848edb8feb0050c4f2c7bf7ac8
GitIrsh <irshuirshath007@gmail.com>
2022-08-30 21:45:18 +0530

    first commit

---
+++ router/router.js
@@ -0,0 +1,17 @@
+const loginRoute = require('../src/controller/controller')
+
+module.exports = [
+
+{
+    method: 'POST',
+    path: '/api/auth/signup',
+    handler: loginRoute.signup
+},
+
+{
+    method: 'POST',
+    path: '/api/auth/signin',
+    handler: loginRoute.signin
+}
+
+]
